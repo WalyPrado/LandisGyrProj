f6dce3ef6c48ca909acaa0a0235058ae3147a6da
Walyson do Prado Fernandes <71782069+WalyPrado@users.noreply.github.com>
2025-01-17 21:55:56 -0300

    correcting class

--- LandisGyrProj/Classes/Endpoint.cs
+++ LandisGyrProj/Classes/Endpoint.cs
@@ -5,9 +5,9 @@ namespace LandisGyrProj.Classes
     public class Endpoint
     {
         public string endpointSerialNumber { get; set; }
-        public int meterModelId { get; set; }
+        public MeterModel meterModelId { get; set; }
         public int meterNumber { get; set; }
-        public int meterFirmwareVersion { get; set; }
+        public string meterFirmwareVersion { get; set; }
         public State switchState { get; set; }
 
         public Endpoint()
@@ -15,7 +15,7 @@ namespace LandisGyrProj.Classes
             endpointSerialNumber = string.Empty;
             meterModelId = 0;
             meterNumber = 0;
-            meterFirmwareVersion = 0;
+            meterFirmwareVersion = string.Empty;
             switchState = 0;
         }
     }
